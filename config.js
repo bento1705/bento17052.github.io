a84274d4d04fdb1f2b5af1e98eeef07fbf77719b
bento1705 <tuanthanh17052002@gmail.com>
2022-02-05 20:50:56 +0700

    upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Dear bé Tùng :>',
+    introDesc: `sau đây là lời nhắn \n
+     Chú Thành dành đến bé Tùng\n  
+    Nhưng theo một cách hơi cồng kềnh =)))`,
+    btnIntro: 'click here 👋🏻',
+    title: 'Mai trời lạnh phết nèe, e đón đi ăn món gì nóng nóng nhaaa ^^ ',
+    desc: 'Anh có thể chọn "Không" :(...Nếu anh có thể =)))))',
+    btnYes: 'Đi luôn ngại gì =)) 🥰',
+    btnNo: 'Không! Đi ăn một mình đi 😏',
+    question:'One more question!! who is the most gorgeous panda in the world?',
+    btnReply: 'Send it to Thành',
+    reply: 'bé Tùng :> ',
+    mess: 'hừ, biết ngay mà 🙃 nhưng mà thôi đúng quá nên cũng không cãi được =))) ',
+    messDesc: 'bé còn chờ gì nữa =))) direct ngay cho chú nèo 😝',
+    btnAccept: 'Lets gooo!!',
+    messLink: 'https://www.instagram.com/bento17_05/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
